3bc74ac97b13e47aadeac1a8f63e556cf2f7356a
Toma Puljak <toma.puljak@hotmail.com>
2025-08-14 13:13:39 +0000

    fix: revert
    Signed-off-by: Toma Puljak <toma.puljak@hotmail.com>

--- apps/api/src/sandbox/dto/sandbox.dto.ts
+++ apps/api/src/sandbox/dto/sandbox.dto.ts
@@ -316,9 +316,6 @@ export class SandboxDto {
           return SandboxState.CREATING
         }
         break
-      case SandboxState.PENDING_ARCHIVE:
-        // @ts-expect-error - this is a temporary state
-        return 'archiving'
     }
     return sandbox.state
   }
