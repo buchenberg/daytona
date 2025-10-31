02cd30792757056a022b5ae9f79bef5cd3c20bb6
Vedran Jukic <vedran.jukic@gmail.com>
2025-10-31 17:07:47 +0100

    refactor(api): sandbox locks and pending property (#2756)
    Signed-off-by: Vedran Jukic <vedran.jukic@gmail.com>
    Signed-off-by: MDzaja <mirkodzaja0@gmail.com>

--- apps/api/src/sandbox/services/runner.service.ts
+++ apps/api/src/sandbox/services/runner.service.ts
@@ -123,6 +123,10 @@ export class RunnerService {
         : MoreThanOrEqual(this.configService.getOrThrow('runnerUsage.availabilityScoreThreshold')),
     }
 
+    const excludedRunnerIds = params.excludedRunnerIds?.length
+      ? params.excludedRunnerIds.filter((id) => !!id)
+      : undefined
+
     if (params.snapshotRef !== undefined) {
       const snapshotRunners = await this.snapshotRunnerRepository.find({
         where: {
@@ -133,8 +137,8 @@ export class RunnerService {
 
       let runnerIds = snapshotRunners.map((snapshotRunner) => snapshotRunner.runnerId)
 
-      if (params.excludedRunnerIds?.length) {
-        runnerIds = runnerIds.filter((id) => !params.excludedRunnerIds.includes(id))
+      if (excludedRunnerIds?.length) {
+        runnerIds = runnerIds.filter((id) => !excludedRunnerIds.includes(id))
       }
 
       if (!runnerIds.length) {
@@ -142,8 +146,8 @@ export class RunnerService {
       }
 
       runnerFilter.id = In(runnerIds)
-    } else if (params.excludedRunnerIds?.length) {
-      runnerFilter.id = Not(In(params.excludedRunnerIds))
+    } else if (excludedRunnerIds?.length) {
+      runnerFilter.id = Not(In(excludedRunnerIds))
     }
 
     if (params.region !== undefined) {
