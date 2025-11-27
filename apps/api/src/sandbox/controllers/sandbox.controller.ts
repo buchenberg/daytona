b33abd18ea1bdf3030f7ab14f77bb6ea52cefe6b
Ivan Dagelic <dagelic.ivan@gmail.com>
2025-11-27 12:10:10 +0100

    feat(api): add error reason to sandbox update endpoint (#3028)
    Signed-off-by: Ivan Dagelic <dagelic.ivan@gmail.com>

--- apps/api/src/sandbox/controllers/sandbox.controller.ts
+++ apps/api/src/sandbox/controllers/sandbox.controller.ts
@@ -559,7 +559,7 @@ export class SandboxController {
     @Param('sandboxId') sandboxId: string,
     @Body() updateStateDto: UpdateSandboxStateDto,
   ): Promise<void> {
-    await this.sandboxService.updateState(sandboxId, updateStateDto.state)
+    await this.sandboxService.updateState(sandboxId, updateStateDto.state, updateStateDto.errorReason)
   }
 
   @Post(':sandboxIdOrName/backup')
