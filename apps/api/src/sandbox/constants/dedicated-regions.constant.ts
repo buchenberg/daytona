075568f51c4d214613774c537ca2f609f9d356d5
fabjanvucina <fabjanvucina@gmail.com>
2025-12-03 13:11:17 +0000

    chore: add fabjan@daytona.io personal org to large sandbox orgs list
    Signed-off-by: fabjanvucina <fabjanvucina@gmail.com>

--- apps/api/src/sandbox/constants/dedicated-regions.constant.ts
+++ apps/api/src/sandbox/constants/dedicated-regions.constant.ts
@@ -61,6 +61,7 @@ export const LARGE_SANDBOX_ORGS = new Set([
   '50071d40-742e-4d9d-be5c-b857493148f7', // idler
   '2f116a7c-d116-45db-9863-faa2ba6171a0', // Finarth.AI
   '287d67b2-f117-49b9-8ae6-50df214ab964', // Gel
+  '9f4f4bb5-a521-47a2-9263-462dc409db1d', // fabjan@daytona.io personal org
 ])
 
 /*
