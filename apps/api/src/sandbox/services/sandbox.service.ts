1ce7e62ef3586e8d13a3692003e1197ccbb0cac6
Ivan Dagelic <dagelic.ivan@gmail.com>
2026-02-03 13:27:02 +0100

    fix(api): message on errored sandbox actions (#3583)
    Signed-off-by: Ivan Dagelic <dagelic.ivan@gmail.com>

--- apps/api/src/sandbox/services/sandbox.service.ts
+++ apps/api/src/sandbox/services/sandbox.service.ts
@@ -114,6 +114,12 @@ export class SandboxService {
     return `sandbox:${id}:state-change-sync`
   }
 
+  private assertSandboxNotErrored(sandbox: Sandbox): void {
+    if ([SandboxState.ERROR, SandboxState.BUILD_FAILED].includes(sandbox.state)) {
+      throw new SandboxError('Sandbox is in an errored state')
+    }
+  }
+
   private async validateOrganizationQuotas(
     organization: Organization,
     regionId: string,
@@ -261,6 +267,8 @@ export class SandboxService {
 
   async archive(sandboxIdOrName: string, organizationId?: string): Promise<Sandbox> {
     const sandbox = await this.findOneByIdOrName(sandboxIdOrName, organizationId)
+
+    this.assertSandboxNotErrored(sandbox)
 
     if (String(sandbox.state) !== String(sandbox.desiredState)) {
       throw new SandboxError('State change in progress')
@@ -1216,6 +1224,8 @@ export class SandboxService {
         return sandbox
       }
 
+      this.assertSandboxNotErrored(sandbox)
+
       if (String(sandbox.state) !== String(sandbox.desiredState)) {
         // Allow start of stopped | archived and archiving | archived sandboxes
         if (
@@ -1279,6 +1289,8 @@ export class SandboxService {
 
   async stop(sandboxIdOrName: string, organizationId?: string): Promise<Sandbox> {
     const sandbox = await this.findOneByIdOrName(sandboxIdOrName, organizationId)
+
+    this.assertSandboxNotErrored(sandbox)
 
     if (String(sandbox.state) !== String(sandbox.desiredState)) {
       throw new SandboxError('State change in progress')
