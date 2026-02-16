9b9f442f692fc695cc6c6bf365b44e227bcd724e
Ivan Dagelic <dagelic.ivan@gmail.com>
2026-02-16 18:14:37 +0100

    feat(api): get sandbox org id cache (#3777)
    Signed-off-by: Ivan Dagelic <dagelic.ivan@gmail.com>

--- apps/api/src/sandbox/services/sandbox.service.ts
+++ apps/api/src/sandbox/services/sandbox.service.ts
@@ -82,8 +82,11 @@ import { InjectRedis } from '@nestjs-modules/ioredis'
 import { Redis } from 'ioredis'
 import {
   SANDBOX_LOOKUP_CACHE_TTL_MS,
+  SANDBOX_ORG_ID_CACHE_TTL_MS,
   sandboxLookupCacheKeyById,
   sandboxLookupCacheKeyByName,
+  sandboxOrgIdCacheKeyById,
+  sandboxOrgIdCacheKeyByName,
 } from '../utils/sandbox-lookup-cache.util'
 import { SandboxLookupCacheInvalidationService } from './sandbox-lookup-cache-invalidation.service'
 
@@ -622,6 +625,14 @@ export class SandboxService {
 
     const result = await this.sandboxRepository.save(warmPoolSandbox)
 
+    // Defensive invalidation of orgId cache since the sandbox moved from unassigned to a real organization
+    this.sandboxLookupCacheInvalidationService.invalidateOrgId({
+      sandboxId: warmPoolSandbox.id,
+      organizationId: organization.id,
+      name: warmPoolSandbox.name,
+      previousOrganizationId: SANDBOX_WARM_POOL_UNASSIGNED_ORGANIZATION,
+    })
+
     // Treat this as a newly started sandbox
     this.eventEmitter.emit(
       SandboxEvents.STATE_UPDATED,
@@ -1071,7 +1082,10 @@ export class SandboxService {
         ...(organizationId ? { organizationId: organizationId } : {}),
       },
       select: ['organizationId'],
-      loadEagerRelations: false,
+      cache: {
+        id: sandboxOrgIdCacheKeyById({ organizationId, sandboxId: sandboxIdOrName }),
+        milliseconds: SANDBOX_ORG_ID_CACHE_TTL_MS,
+      },
     })
 
     if (!sandbox && organizationId) {
@@ -1081,7 +1095,10 @@ export class SandboxService {
           organizationId: organizationId,
         },
         select: ['organizationId'],
-        loadEagerRelations: false,
+        cache: {
+          id: sandboxOrgIdCacheKeyByName({ organizationId, sandboxName: sandboxIdOrName }),
+          milliseconds: SANDBOX_ORG_ID_CACHE_TTL_MS,
+        },
       })
     }
 
