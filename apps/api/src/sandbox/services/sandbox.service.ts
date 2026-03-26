df0183e2368662f13c7440c05d48cf1c22399819
Toma Puljak <toma.puljak@hotmail.com>
2026-03-26 14:55:49 +0100

    feat(api,runner,sdk): force stop sandbox (#4172)
    Signed-off-by: Toma Puljak <toma.puljak@hotmail.com>

--- apps/api/src/sandbox/services/sandbox.service.ts
+++ apps/api/src/sandbox/services/sandbox.service.ts
@@ -1367,7 +1367,7 @@ export class SandboxService {
     }
   }
 
-  async stop(sandboxIdOrName: string, organizationId?: string): Promise<Sandbox> {
+  async stop(sandboxIdOrName: string, organizationId?: string, force?: boolean): Promise<Sandbox> {
     const sandbox = await this.findOneByIdOrName(sandboxIdOrName, organizationId)
 
     this.assertSandboxNotErrored(sandbox)
@@ -1397,7 +1397,7 @@ export class SandboxService {
     if (sandbox.autoDeleteInterval === 0) {
       this.eventEmitter.emit(SandboxEvents.DESTROYED, new SandboxDestroyedEvent(updatedSandbox))
     } else {
-      this.eventEmitter.emit(SandboxEvents.STOPPED, new SandboxStoppedEvent(updatedSandbox))
+      this.eventEmitter.emit(SandboxEvents.STOPPED, new SandboxStoppedEvent(updatedSandbox, force))
     }
 
     return updatedSandbox
