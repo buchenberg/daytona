a00a6edf269164a6f727fc23d0c38bc98d6a5ba9
Robert Pavlinic <26637497+rpavlini@users.noreply.github.com>
2026-05-13 09:56:34 +0200

    chore(dashboard): clean up unused FFs (#4689)
    Signed-off-by: rpavlini <rpavlinic@daytona.io>

--- apps/dashboard/src/enums/FeatureFlags.ts
+++ apps/dashboard/src/enums/FeatureFlags.ts
@@ -6,11 +6,7 @@
 export enum FeatureFlags {
   ORGANIZATION_INFRASTRUCTURE = 'organization_infrastructure',
   ORGANIZATION_EXPERIMENTS = 'organization_experiments',
-  DASHBOARD_PLAYGROUND = 'dashboard_playground',
-  DASHBOARD_FILESYSTEM = 'dashboard_filesystem',
-  DASHBOARD_WEBHOOKS = 'dashboard_webhooks',
   SANDBOX_SPENDING = 'sandbox_spending',
-  DASHBOARD_CREATE_SANDBOX = 'dashboard_create-sandbox',
   SANDBOX_LINUX_VM = 'sandbox_linux_vm',
   STRIPE_ABUSE_VERIFICATION = 'stripe_abuse_verification',
 }
