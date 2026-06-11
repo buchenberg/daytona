c542a504a87e0d6d4df7b1d83e40c55a859ac9da
Toma Puljak <toma.puljak@hotmail.com>
2026-06-11 14:35:03 +0000

    chore: update push-dashboard.sh
    Signed-off-by: Toma Puljak <toma.puljak@hotmail.com>

--- push-dashboard.sh
+++ push-dashboard.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-aws s3 sync dist/apps/dashboard/ s3://prod-daytona-dashboard
+## If serving dashboard from /
+# aws s3 sync dist/apps/dashboard/ s3://prod-daytona-dashboard
+# aws cloudfront create-invalidation --distribution-id E3GMKPSVYPDR26 --paths "/*"
+
+## If serving dashboard from /dashboard - make sure to build docs with VITE_BASE_PATH=/dashboard/ 
+aws s3 sync dist/apps/dashboard/ s3://prod-daytona-dashboard/dashboard --delete
 aws cloudfront create-invalidation --distribution-id E3GMKPSVYPDR26 --paths "/*"
