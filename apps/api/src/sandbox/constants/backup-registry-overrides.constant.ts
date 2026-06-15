a7f599dc22ff768cb916e38a989abc95163aac56
Ivan Dagelic <dagelic.ivan@gmail.com>
2026-06-15 13:04:44 +0000

    hah(api): temp scrapybara backup override
    Signed-off-by: Ivan Dagelic <dagelic.ivan@gmail.com>

--- apps/api/src/sandbox/constants/backup-registry-overrides.constant.ts
+++ apps/api/src/sandbox/constants/backup-registry-overrides.constant.ts
@@ -13,9 +13,23 @@
  * The key is the organization ID and the value is the docker registry ID.
  */
 export const BACKUP_REGISTRY_OVERRIDES_PER_ORGANIZATION: Record<string, string> = {
-  '55717397-f840-4f5b-a829-77fd6f7cb2fc': '360ca1bc-6e41-4aa4-9972-206c8997abee',
+  // '55717397-f840-4f5b-a829-77fd6f7cb2fc': '360ca1bc-6e41-4aa4-9972-206c8997abee',
 }
 
 export function getBackupRegistryOverride(organizationId: string): string | undefined {
-  return BACKUP_REGISTRY_OVERRIDES_PER_ORGANIZATION[organizationId]
+  if (organizationId != '55717397-f840-4f5b-a829-77fd6f7cb2fc') {
+    return undefined
+  }
+
+  if (Math.random() < 0.5) {
+    return '360ca1bc-6e41-4aa4-9972-206c8997abee'
+  } else {
+    if (Math.random() < 0.5) {
+      return '20da41eb-6031-4d3a-849f-813024bebd60'
+    } else {
+      return '623bc3ad-4709-4f20-a795-f62fa1de5caa'
+    }
+  }
+
+  // return BACKUP_REGISTRY_OVERRIDES_PER_ORGANIZATION[organizationId]
 }
