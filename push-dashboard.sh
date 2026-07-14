4f1406284e3fae05dc9a3c1830ec1f3eec18a8b4
Tpuljak <toma.puljak@hotmail.com>
2026-07-14 07:43:49 +0000

    chore: add eu dashboard bucket update to push
    Signed-off-by: Tpuljak <toma.puljak@hotmail.com>

--- push-dashboard.sh
+++ push-dashboard.sh
@@ -4,6 +4,8 @@
 # aws s3 sync dist/apps/dashboard/ s3://prod-daytona-dashboard
 # aws cloudfront create-invalidation --distribution-id E3GMKPSVYPDR26 --paths "/*"
 
-## If serving dashboard from /dashboard - make sure to build docs with VITE_BASE_PATH=/dashboard/ 
+## If serving dashboard from /dashboard - make sure to build docs with VITE_BASE_PATH=/dashboard/
 aws s3 sync dist/apps/dashboard/ s3://prod-daytona-dashboard/dashboard --delete
 aws cloudfront create-invalidation --distribution-id E3GMKPSVYPDR26 --paths "/*"
+
+aws s3 sync dist/apps/dashboard/ s3://daytona-eu-hah/dashboard --region eu-central-1
